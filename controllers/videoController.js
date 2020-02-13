164bf7016905832c84200da9cece539d4e4cccb7
hanbi97 <rain7i@naver.com>
2020-02-13 20:05:47 +0900

    [MODIFY] search

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,6 +1,17 @@
 export const home = (req,res) => res.render("home");
-export const search = (req,res) => res.render("search");
+
+export const search = (req,res) => {
+    const {
+        query:{term : searchingBy}
+    } =req;
+    // const searchingBy = req.query.term;
+    res.render("search", {pageTitle: "Search", searchingBy: searchingBy});
+}
+
 export const upload = (req,res) => res.render("upload");
+
 export const videoDetail = (req,res) => res.render("videoDetail");
+
 export const editVideo = (req,res) => res.render("editVideo");
+
 export const deleteVideo = (req,res) => res.render("deleteVideo");
