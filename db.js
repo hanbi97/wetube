871a89c544d8af8773bdd6b605e5f231a7c643ea
hanbi97 <rain7i@naver.com>
2020-02-14 01:17:28 +0900

    [CREATE] fakedb

---
+++ db.js
@@ -0,0 +1,38 @@
+export const videos=[
+    {
+        id: 12334,
+        title: 'myvideo',
+        description: 'this is mine.',
+        views: 24,
+        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+          id: 1212,
+          name: "Bb",
+          email: "Bb@aaa.com"
+        } 
+    },
+    {
+        id: 12335,
+        title: 'myvideo1',
+        description: 'this is mine.',
+        views: 24,
+        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+          id: 121,
+          name: "Bb",
+          email: "Bb@aaa.com"
+        } 
+    },
+    {
+        id: 12336,
+        title: 'myvideo2',
+        description: 'this is mine.',
+        views: 24,
+        videoFile: "https://ia800300.us.archive.org/17/items/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+        creator: {
+          id: 112,
+          name: "Bb",
+          email: "Bb@aaa.com"
+        } 
+    }
+];
