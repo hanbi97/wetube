3daf5123ef8daf0a0f56fd0c65d9bdd5c4ad7531
hanbi97 <rain7i@naver.com>
2020-02-13 17:53:35 +0900

    [MODIFY] app.js

--- app.js
+++ app.js
@@ -3,19 +3,21 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { localsMiddleware } from "./middlewares";
 import userRouter from "./routers/userRouter"; //default로 export 안해서 이렇게 함
 import videoRouter from "./routers/videoRouter"; //default로 export 안해서 이렇게 함
 import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 const app = express(); // make an express application
 
+app.use(helmet());
 app.set('view engine','pug');
-app.set('views', './views');
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-app.use(helmet());
 app.use(morgan("dev"));
+
+app.use(localsMiddleware);
 
 app.use(routes.home,globalRouter);
 app.use(routes.users,userRouter); 
