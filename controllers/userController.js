86c95ea210d19123de30a7828e2e7f861bfdea08
hanbi97 <rain7i@naver.com>
2020-02-15 23:50:47 +0900

    [MODIFY] add function for userDetail and videoDetail with id

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,33 @@
-export const join =(req,res)=>res.render("join");
-export const login=(req,res)=>res.render("login");
+import routes from "../routes";
+
+export const getJoin =(req,res)=>{
+    res.render("join");
+};
+
+export const postJoin =(req,res)=>{
+    //get join information
+    const{
+        body: {name, email, password, password2}
+    }=req;
+    //check the password == password2
+    if(password!=password2){
+        res.status(400);
+    }
+    else{
+        //to do: register user, user login
+        res.redirect(routes.home);
+    }
+    res.render("join");
+};
+
+export const getLogin=(req,res)=>{
+    res.render("login");
+}
+export const postLogin=(req,res)=>{
+    //to do: check users's password in database
+    res.redirect(routes.home);
+};
+
 export const logout=(req,res)=>res.render("logout");
 export const userDetail = (req,res)=>res.render("userDetail");
 export const editProfile = (req,res)=>res.render("editProfile");
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -7,7 +7,7 @@ const videoRouter = express.Router();
 //videoRouter.get(routes.videos,videos);
 videoRouter.get(routes.upload,upload);
 videoRouter.get(routes.editVideo,editVideo);
-videoRouter.get(routes.videoDetail,videoDetail);
+videoRouter.get(routes.videoDetail(),videoDetail);
 videoRouter.get(routes.deleteVideo,deleteVideo);
 
 export default videoRouter;
