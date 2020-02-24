fce32b20e18936eed4d55a2799705122a70e0db0
hanbi97 <rain7i@naver.com>
2020-02-24 18:09:59 +0900

    [MODIFY] security

--- init.js
+++ init.js
@@ -1,7 +1,9 @@
 import app from "./app"; //default로 export시 이렇게 가능
 import "./db";
+import dotenv from "dotenv";
+dotenv.config();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const handleListening =()=>console.log(`Listening on: http://localhost:${PORT}`);
 
