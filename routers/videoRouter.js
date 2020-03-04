6384559bcd399f7beac8f5b5083806d8bdad5eac
hanbi97 <rain7i@naver.com>
2020-03-04 23:51:22 +0900

    [MODIFY]

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import routes from "../routes";
-import {videoDetail, editVideo, deleteVideo, getUpload, postUpload } from "../controllers/videoController";
+import {videoDetail, deleteVideo, getUpload, postUpload, getEditVideo, postEditVideo } from "../controllers/videoController";
 import { uploadVideoMiddleware } from "../middlewares";
 
 const videoRouter = express.Router();
@@ -9,7 +9,9 @@ const videoRouter = express.Router();
 videoRouter.get(routes.upload,getUpload);
 videoRouter.post(routes.upload,uploadVideoMiddleware, postUpload);
 
-videoRouter.get(routes.editVideo,editVideo);
+videoRouter.get(routes.editVideo(),getEditVideo);
+videoRouter.post(routes.editVideo(),postEditVideo);
+
 videoRouter.get(routes.videoDetail(),videoDetail);
 videoRouter.get(routes.deleteVideo,deleteVideo);
 
