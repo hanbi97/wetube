b6d628e819a6652f5258f36a4fa62915f1bcf779
hanbi97 <rain7i@naver.com>
2020-03-11 20:31:33 +0900

    [MODIFY]

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ const MODE = process.env.WEBPACK_ENV;
 const ENTRY_FILE = path.resolve(__dirname,"assets","js","main.js");
 const OUTPUT_DIR = path.join(__dirname,"static");
 const config ={
-    entry:["@babel/polyfill",ENTRY_FILE],
+    entry:["@babel/polyfill",ENTRY_FILE], //to use babel
     mode:MODE,
     module:{
         //if find module, follow the rules below
